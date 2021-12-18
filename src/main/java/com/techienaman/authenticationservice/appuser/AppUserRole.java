289f909db539d2e514ffffda0cf6e80831c4ae8c
Naman Kumar <namant744@gmail.com>
2021-12-19 00:28:17 +0530

    auth-service initail setup with spring security

---
+++ src/main/java/com/techienaman/authenticationservice/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.techienaman.authenticationservice.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/com/techienaman/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techienaman.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
